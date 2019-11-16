df358bb8cc122bec6344eac6e619956757217a8c
SaraGhahn <55792514+SaraGhahn@users.noreply.github.com>
2019-11-16 17:55:04 +0100

    JS knap

---
+++ buttoncolor.js
@@ -0,0 +1,5 @@
+let bntUdendørs = document.querySelector('#udendørs');
+let bntIndendørs = document.querySelector('#indendørs');
+
+bntUdendørs.addEventListener('click', ()=>bntUdendørs.style.backgroundColor='#4Ba6ac')
+bntIndendørs.addEventListener('click', ()=>bntIndendørs.style.backgroundColor='#4Ba6ac')
